a817d2cabdf4156ffc6f5ced53fd20fee842c4cc
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2019-01-27 15:16:28 -0800

    Third commit: search bar still not working

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,6 +5,7 @@ class SearchBar extends Component {
   constructor(props){
     super(props)
     this.state = {};
+    this.state.searchTerm = '';
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
@@ -13,14 +14,14 @@ class SearchBar extends Component {
     this.setState({search: e.target.value});
   }
   handleTermChange(e){
-    //this.setState(this.state.search = e.target.value);
-    this.props.onSearch(this.state.search);
+    this.setState({searchTerm: e.target.value});
+    //this.props.onSearch(this.state.searchTerm);
   }
   render() {
     return (
       <div className="SearchBar">
   <input placeholder="Enter A Song, Album, or Artist" onChange = {this.handleTermChange}/>
-  <a>SEARCH</a>
+  <a onClick = {this.props.onSearch(this.state.searchTerm)}>SEARCH</a>
 </div>
 );
 }
